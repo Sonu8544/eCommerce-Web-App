f09ac977dda7cb59b04e6c179300a4d51b2cd98d
Sonu Kumar <sksonu8544@gmail.com>
2023-12-30 06:05:50 +0530

    DEV Navbar component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-   <h1>Lorem ipsum dolor sit.</h1>
-   </>
+    <>
+      <Navbar />
+    </>
   )
 }
 
