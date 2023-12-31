6d0b726e47d555b9ea7d85fb42d3c0f9073aba1a
Sonu Kumar <sksonu8544@gmail.com>
2023-12-31 10:03:34 +0530

    DEV item component and popular item

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Item.css'
+
+const Item = (props) => {
+    return (
+        <>
+            <section className="item">
+                <img src={props.image} alt="" />
+                <p>{props.name}</p>
+                <div className="item-prices">
+                    <div className="item-price-new">
+                        Rs: {props.new_price}
+                    </div>
+                    <div className="item-price-old">
+                        Rs: {props.old_price}
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Item
