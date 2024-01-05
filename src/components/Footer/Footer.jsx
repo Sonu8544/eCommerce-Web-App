ae088e4154c2e80d4d7b7662beedf3535b833791
Sonu Kumar <sksonu8544@gmail.com>
2024-01-05 06:56:37 +0530

    change alignment footer and news letter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
     return (<>
@@ -18,7 +19,7 @@ const Footer = () => {
                             </div>
                             {/* Social icon */}
                             <div class="flex -mx-2 items-center mt-5">
-                                <a href="#" class=" item mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-yellow-500 dark:hover:text-yellow-400" aria-label="Reddit">
+                                <a href="#" class=" item mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 " aria-label="Reddit">
                                     <svg class="w-10 h-10 fill-current" viewBox="0 0 24 24" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path
@@ -27,8 +28,8 @@ const Footer = () => {
                                     </svg>
                                 </a>
 
-                                <a href="#" class=" item mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-yellow-500 dark:hover:text-yellow-400" aria-label="Facebook">
-                                    <svg class="w-10 h-10 fill-current" viewBox="0 0 24 24" fill="none"
+                                <a href="#" class=" item mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 " aria-label="Facebook">
+                                    <svg class="w-10 h-10 fill-current " viewBox="0 0 24 24" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path
                                             d="M2.00195 12.002C2.00312 16.9214 5.58036 21.1101 10.439 21.881V14.892H7.90195V12.002H10.442V9.80204C10.3284 8.75958 10.6845 7.72064 11.4136 6.96698C12.1427 6.21332 13.1693 5.82306 14.215 5.90204C14.9655 5.91417 15.7141 5.98101 16.455 6.10205V8.56104H15.191C14.7558 8.50405 14.3183 8.64777 14.0017 8.95171C13.6851 9.25566 13.5237 9.68693 13.563 10.124V12.002H16.334L15.891 14.893H13.563V21.881C18.8174 21.0506 22.502 16.2518 21.9475 10.9611C21.3929 5.67041 16.7932 1.73997 11.4808 2.01722C6.16831 2.29447 2.0028 6.68235 2.00195 12.002Z">
@@ -36,7 +37,7 @@ const Footer = () => {
                                     </svg>
                                 </a>
 
-                                <a href="#" class=" item mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-yellow-500 dark:hover:text-yellow-400" aria-label="Github">
+                                <a href="#" class=" item mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 " aria-label="Github">
                                     <svg class="w-10 h-10 fill-current" viewBox="0 0 24 24" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path
@@ -72,7 +73,7 @@ const Footer = () => {
 
                     <div class="flex justify-center items-center">
                         <a href="#">
-                            <p className='text-red-500' >2024 &#169;sksonu8544@gmail.com</p>
+                            <p className='text-white' > &#169;sksonu8544@gmail.com 2024</p>
                         </a>
                     </div>
                 </div>
