4685bc7a3cc9ec613875cb3b6f4bd796114fcc27
Sonu Kumar <sksonu8544@gmail.com>
2024-01-06 09:06:50 +0530

    filter product by catogiry

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import ShopCategory from './pages/ShopCategory'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
 import LoginSignup from './pages/LoginSignup'
+import men_banner from './components/Assets/banner_mens.png'
+import women_banner from './components/Assets/banner_women.png'
+import kids_banner from './components/Assets/banner_kids.png'
 
 
 function App() {
@@ -16,9 +19,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory category="men"/>} />
-          <Route path='/women' element={<ShopCategory category="women"/>} />
-          <Route path='/kids' element={<ShopCategory category="kids"/>} />
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>} />
+          <Route path='/women' element={<ShopCategory banner={women_banner} category="women"/>} />
+          <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>} />
 
           <Route path='/product' element={<Product />}>
             <Route path=':ProductId' element={<Product />} />
